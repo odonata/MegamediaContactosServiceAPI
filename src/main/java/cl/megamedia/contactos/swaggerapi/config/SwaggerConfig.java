ba3143584546b78adf4cf9101fefe6db9a7352c7
odonata <psygos@gmail.com>
2024-07-24 03:03:20 -0400

    FIN DE PRUEBAS CON API KLEY
    FIN DE PRUEBAS CON API KLEY

--- src/main/java/cl/megamedia/contactos/swaggerapi/config/SwaggerConfig.java
+++ src/main/java/cl/megamedia/contactos/swaggerapi/config/SwaggerConfig.java
@@ -19,6 +19,12 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+/**
+ * Configuración end points Swagger
+ * 
+ * @author Gonzalo Silbva
+ *
+ */
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig implements WebMvcConfigurer{
